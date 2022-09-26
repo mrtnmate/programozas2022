ec188d989526234873c69fa54ff05e1ca65e1ae1
mrtnmate <marton.mate.18@ady-nagyatad.hu>
2022-09-26 09:39:42 +0200

    Üzemanyag 4. feladat

---
+++ Uzemanyag/Uzemanyag/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+feladat f = new feladat();
+class feladat
+{
+    List<adatok> arvaltozas = new List<adatok>();
+    public feladat()
+    {
+        f2();
+        f3();
+        f4();
+        f5();
+    }
+    void f5()
+    {
+
+    }
+    void f4()
+    {
+        int mink = arvaltozas[0].kulonbseg();
+        for (int i = 0; i < arvaltozas.Count; i++)
+        {
+            if (arvaltozas[i].kulonbseg()<mink)
+            {
+                mink = arvaltozas[i].kulonbseg();
+            }
+        }
+        Console.WriteLine("4. feladat: A legkisebb különbség: {0}",mink);
+    }
+    void f3()
+    {
+        Console.WriteLine("3.feladat: Változások száma: {0}",arvaltozas.Count);
+    }
+    void f2()
+    {
+        string[] sorok = File.ReadAllLines("uzemanyag.txt");
+        for (int i = 0; i < sorok.Length; i++)
+        {
+            arvaltozas.Add(new adatok(sorok[i]));
+        }
+    }
+}
+
+class adatok
+{
+    public string datum;
+    public int benzinar, gazolajar;
+    public adatok(string sor)
+    {
+        string[] vag = sor.Split(";");
+        datum = vag[0];
+        benzinar = Convert.ToInt32(vag[1]);
+        gazolajar = Convert.ToInt32(vag[2]);
+    }
+    public int kulonbseg()
+    {
+        return Math.Abs(benzinar - gazolajar);
+    }
+}
--- cbradio/cbradio/Program.cs
+++ cbradio/cbradio/Program.cs
@@ -1,1 +1,61 @@
-﻿
+﻿feladat f = new feladat();
+
+class feladat
+{
+    List<adatok> adasok = new List<adatok>();
+    public feladat()
+    {
+        f2();
+        f3();
+        f4();
+    }
+
+    void f2()
+    {
+        string[] sorok = File.ReadAllLines("cb.txt");
+        for (int i = 1; i < sorok.Length; i++)
+        {
+            adasok.Add(new adatok(sorok[i]));
+        }
+    }
+
+    void f3()
+    {
+        Console.WriteLine("3.feladat: Bejegyzések száma: {0}db",adasok.Count);
+    }
+
+    void f4()
+    {
+        bool volt=false;
+        for (int i = 0; i < adasok.Count; i++)
+        {
+            if (adasok[i].adasDb == 4)
+            {
+                volt = true;
+
+                break;
+            }
+
+        }
+        if (volt)
+        {
+                Console.WriteLine("4.feladat: Volt négy adást indító söfőr");
+            
+        }
+    }
+}
+
+
+class adatok
+{
+    public int ora, perc, adasDb;
+    public string nev;
+    public adatok(string sor)
+    {
+        string[] vag = sor.Split(";");
+        ora=Convert.ToInt32(vag[0]);
+        perc=Convert.ToInt32(vag[1]);
+        adasDb=Convert.ToInt32(vag[2]);
+        nev=vag[3];
+    }
+}
